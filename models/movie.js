58a346ae2721765b33fba43aab4b1e0149012bac
xxx <chuxuguan1315@163.com>
2016-08-24 18:07:43 +0800

    注册登入功能完成，页面结构调整

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var MovieSchema = require('../schemas/movie');
 var Movie = mongoose.model('Movie',MovieSchema);
+var cookieSession = require('cookie-session');
 
 module.exports = Movie
