179128f8811fbbfd9a99b22e0d3031dc97186fb9
xxx <chuxuguan1315@163.com>
2016-08-24 15:33:08 +0800

    zancun

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var path=require('path');
 var port=process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var Movie = require('./models/movie');
-var _user = require('./models/user');
+var User = require('./models/user');
 var _=require('underscore');
 var app =express();
 var serverStatic=require('serve-static');
 var bodyParser=require('body-parser');
-
+var cookieSession = require('cookie-session');
 mongoose.connect('mongodb://localhost/imooc')
 
 app.use(serverStatic('public'));
+app.use(cookieSession({
+	secret:'imooc',
+	esave:false,
+	saveUninitialized:true
+}))
 //app.use(express.static(path.join(__dirname,'public')))
 app.set('views','./views/pages');
 app.set('view engine','jade');
@@ -21,6 +26,7 @@ console.log('port'+port);
 
 // index page
 app.get('/',function(req,res){
+	console.log(req.session.user)
 	Movie.fetch(function(err,movies){
 		if(err){
 			console.log(err)
@@ -45,8 +51,61 @@ app.get('/movie/:id',function(req,res){
 //signup
 app.post('/user/signup',function(req,res){
 	var _user = req.body.user;
-	console.log(_user);
-	
+	User.findOne({name:_user.name},function(err,user){
+		if(err){
+			console.log(err);
+		}
+		if(user){
+			res.json({success:0})
+		}else{
+			var user=new User(_user);
+			user.save(function(err,user){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/')
+			})
+		}
+	})
+})
+//signin
+app.post('/user/signin',function(req,res){
+	var _user = req.body.user;
+	var name=_user.name;
+	var password=_user.password;
+
+	User.findOne({name:name},function(err,user){
+		if(err){
+			console.log(err);
+		}
+		if(!user){
+			return res.redirect('/');
+		}
+		user.comparePassword(password,function(err,isMatch){
+			if(err){
+				console.log(err);
+			}
+			if(isMatch){
+				req.session.user= user;
+				return res.redirect('/');
+			}else{
+				console.log('password不匹配')
+			}
+		})
+	})
+})
+// userlist page
+app.get('/admin/userlist',function(req,res){
+	User.fetch(function(err,users){
+			if(err){
+					console.log(err)
+				}
+
+		res.render('userlist',{
+			title:'imooc 用户列表',
+			users:users
+		})
+	})
 })
 // admin page
 app.get('/admin/movie',function(req,res){
